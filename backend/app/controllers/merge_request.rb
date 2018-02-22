39cc025a7f7b0bb15af9bef6915737db2f30fcb7
alyxrossetti <arossetti@getty.edu>
2018-02-22 08:11:32 -0800

    Make all forms work in merge select view. Some minor display issues remain

--- backend/app/controllers/merge_request.rb
+++ backend/app/controllers/merge_request.rb
@@ -257,12 +257,6 @@ class ArchivesSpaceService < Sinatra::Base
       end
     target['title'] = target['names'][0]['sort_name']
     end
-    target = add_victim_related(target, victim)
-    target
-  end
-
-  def add_victim_related(target, victim)
-    target['related_agents'].push(*victim['related_agents'])
     target
   end
 
